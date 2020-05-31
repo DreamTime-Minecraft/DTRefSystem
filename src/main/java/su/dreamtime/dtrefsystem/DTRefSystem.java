8e1f883dff5e53fdf0f01bdcf0d768af85d39932
Muravlev <muravlev@skytecgames.ru>
2020-05-31 11:25:14 +0700

    initial commit

---
+++ src/main/java/su/dreamtime/dtrefsystem/DTRefSystem.java
@@ -0,0 +1,16 @@
+package su.dreamtime.dtrefsystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DTRefSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
